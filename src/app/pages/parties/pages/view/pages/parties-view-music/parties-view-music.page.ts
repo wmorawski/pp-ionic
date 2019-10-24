c9e2248221fecc9bc74ddee846466d9d8aa963c0
Wiktor Morawski <w.morawski@wildasoftware.pl>
2019-10-24 20:37:35 +0200

    Spotify login added

--- src/app/pages/parties/pages/view/pages/parties-view-music/parties-view-music.page.ts
+++ src/app/pages/parties/pages/view/pages/parties-view-music/parties-view-music.page.ts
@@ -1,28 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-import { SpotifyAuth } from '@ionic-native/spotify-auth/ngx';
+import { AuthorizationData } from '@ionic-native/spotify-auth/ngx';
+import { environment } from 'src/environments/environment';
+import { Track, init, getCurrentUserTopTracks } from 'spotify-web-sdk';
 
+declare var cordova: any;
 @Component({
     selector: 'app-parties-view-music',
     templateUrl: './parties-view-music.page.html',
     styleUrls: ['./parties-view-music.page.scss'],
 })
 export class PartiesViewMusicPage implements OnInit {
-    result = {};
-    constructor(private readonly spotifyAuth: SpotifyAuth) {}
+    result: AuthorizationData;
+    topTracks: Track[];
+    constructor() {}
 
     ngOnInit() {}
+    async ionViewWillEnter() {
+        this.result = await this.authWithSpotify();
+        init({ token: this.result.accessToken });
+        this.topTracks = (await getCurrentUserTopTracks({ limit: 50 })).items;
+    }
     authWithSpotify() {
-        const config = {
-            clientId: 'cf6c218047e74336a40b6c1cfe70f35a',
-            redirectUrl: 'partyplanner://callback',
-            scopes: ['streaming', 'playlist-read-private', 'user-read-email', 'user-read-private'],
-            tokenExchangeUrl: 'http://localhost:4000/auth/spotify/exchange',
-            tokenRefreshUrl: 'http://localhost:4000/auth/spotify/refresh',
-        };
-
-        this.spotifyAuth.authorize(config).then(res => {
-            console.log(res);
-            this.result = { access_token: res.accessToken, expires_in: res.expiresAt, ref: res.encryptedRefreshToken };
-        });
+        return cordova.plugins.spotifyAuth.authorize(environment.spotify.config);
     }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,6 +8,22 @@ export const environment = {
     mapbox: {
         accessToken: 'pk.eyJ1IjoicmF0dGVuIiwiYSI6ImNqcnE4eWk0czAwdWI0M3BmOGl2bzd1N3cifQ.N8Eyx2hvwkOGnZhSvttvZw',
     },
+    spotify: {
+        config: {
+            clientId: 'cf6c218047e74336a40b6c1cfe70f35a',
+            redirectUrl: 'partyplanner://callback',
+            scopes: [
+                'user-read-email',
+                'user-read-private',
+                'user-top-read',
+                'playlist-modify-public',
+                'playlist-modify-private',
+                'streaming',
+            ],
+            tokenExchangeUrl: 'auth/spotify/exchange',
+            tokenRefreshUrl: 'auth/spotify/refresh',
+        },
+    },
 };
 
 /*
